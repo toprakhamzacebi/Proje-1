82054f46d987fe5e2f2d88a2ce9e7dcb48452a66
toprakhamzacebi <toprakhamzacebi2007@gmail.com>
2024-05-21 21:33:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Hello World")
+
+
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "VIBE": "Aura, enerji",
+            "TOXIC": "Sağlıksız, zarar verici",
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+#print(word)
+
+if word in meme_dict.keys():
+    print("anlamı =" ,meme_dict[word])
+else:
+    print("Sözlüğünüzde  bu kelime bulunamadı")
+    
